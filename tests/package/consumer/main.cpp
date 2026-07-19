6ae66aeb4877b09fd3ed9e6024de5ee17e4c9dfc
Tetsuya Hayashi <tetsu.h@gmail.com>
2026-07-19 15:12:24 +0900

    test(cache): require removal of AttachBase API (#100)
    - Add negative public-header concepts for the removed AttachBase member
    
    - Apply the contract to build-tree and installed-package consumers

--- tests/package/consumer/main.cpp
+++ tests/package/consumer/main.cpp
@@ -1,5 +1,12 @@
 #include "sitos/sitos.hpp"
 
+template <typename T>
+concept HasAttachBase = requires(T& value) {
+  value.AttachBase();
+};
+
+static_assert(!HasAttachBase<sitos::ParamCache>);
+
 int main() {
   static_cast<void>(sitos::MakeZenohTransport());
   return 0;
